4ad61e9db670c4089f39510fef064b5d96503f12
OliverScherf <scherf.oliver@gmx.net>
2018-03-05 12:14:04 +0100

    [FEAT] finish 1.5

--- assignment1/Assignment1.5.py
+++ assignment1/Assignment1.5.py
@@ -1,5 +1,5 @@
 '''
-Created on 27.02.2018
+Created on 05.03.2018
 
 @author: Matthias Müller-Brockhausen & Oliver Scherf
 '''
@@ -11,6 +11,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn
+import copy
 
 from sklearn import svm, datasets
 from sklearn.model_selection import train_test_split
@@ -19,63 +20,6 @@ from matplotlib.pyplot import plot
 from Utils import plot_confusion_matrix
 from numpy.matlib import rand
 
-STEP_SIZE = 1.0
-
-def maxIndex(vector):
-    max = -sys.maxsize - 1
-    idx = -1
-    for i in range(0, len(vector)):
-        if (vector[i] > max):
-            max = vector[i]
-            idx = i
-    
-    return idx
-
-def classify(img, weights, bias):
-    y = np.zeros(10)
-    for i in range(0, len(y)):
-        y[i] = np.sum(img * weights[i]) + bias[i]
-    return y    
-    
-    
-def classifyAll(trainIn, weights, bias):
-    classified = np.zeros((len(trainIn), 10))
-    for i in range(0, len(trainIn)):
-        classified[i] = classify(trainIn[i], weights, bias)
-    return classified
-
-def updateWeights(img, weights, bias, classified, desired):
-    desired = int(desired)
-    compare = classified[desired]
-    for i in range(0, len(classified)):
-        if (i == desired):
-            weights[i] = weights[i] + STEP_SIZE * img
-        elif (compare < classified[i]):
-            weights[i] = weights[i] - STEP_SIZE * img
-            
-    return weights
-
-def testWith(setIn, setOut, weights, bias):
-    total = 0
-    correct = 0
-    confusionMatrix = np.zeros((10, 10))
-    for i in range(0, len(setIn)):
-        total += 1
-        recognized = maxIndex(classify(setIn[i], weights, bias))
-        
-        confusionMatrix[int(setOut[i])][int(recognized)] += 1
-        if (recognized == setOut[i]):
-            correct += 1
-    
-    plt.figure()
-    plot_confusion_matrix(confusionMatrix, range(0, 10), title="CM for multi class perceptron")
-    plt.savefig("cm_multi_class_perceptron.png")
-    plt.show()
-    print("Correct", correct, "Total", total, "Ratio:", correct/total)
-
-    
-
-
 def sigmoid(n):
     return 1.0/(1.0+np.exp(-n))
 
@@ -83,9 +27,9 @@ def activationValue(x1,x2, weights):
     return sigmoid(np.sum([x1, x2] * weights[0:2]) + weights[2])
         
 def xor_net(x1, x2, weights):
-    hidden1 = activationValue(x1, x2, weights[0:2], weights[2])
-    hidden2 = activationValue(x1, x2, weights[3:5], weights[5])
-    output = activationValue(hidden1, hidden2, weights[6:8], weights[8])
+    hidden1 = activationValue(x1, x2, weights[0:3])
+    hidden2 = activationValue(x1, x2, weights[3:6])
+    output = activationValue(hidden1, hidden2, weights[6:9])
     return output
     
 
@@ -98,25 +42,13 @@ def grdmse(weights):
     xorVector = np.array([0, 1, 1, 0])
     e = 10**-3
     
-    for i in range(0, len(xorVector)):
-        for j in range(0, 6, 3):
-            sliceWeights0 = weights[j:j+3]
-            sliceWeights0[0] += e 
-            sliceWeights1 = weights[j:j+3]
-            sliceWeights1[1] += e 
-            sliceWeights2 = weights[j:j+3]
-            sliceWeights2[2] += e 
-
-            weights[j]     = (activationValue(xMatrix[0][i], xMatrix[1][i], sliceWeights0) 
-                              - activationValue(xMatrix[0][i], xMatrix[1][i], weights[j:j+3])) / e
-            
-            weights[j + 1]     = (activationValue(xMatrix[0][i], xMatrix[1][i], sliceWeights1) 
-                              - activationValue(xMatrix[0][i], xMatrix[1][i], weights[j:j+3])) / e
-            
-            weights[j + 2]     = (activationValue(xMatrix[0][i], xMatrix[1][i], sliceWeights2) 
-                              - activationValue(xMatrix[0][i], xMatrix[1][i], weights[j:j+3])) / e
     
-    return weights
+    returnVector = np.zeros(9)
+    for j in range(0, 9):
+        sliceWeights0 = copy.copy(weights)
+        sliceWeights0[j] += e
+        returnVector[j]   = ((mse(sliceWeights0) - mse(weights)) / e)
+    return returnVector
 
 def mse(weights):
     xMatrix = np.array([[0, 0, 1, 1],
@@ -134,7 +66,14 @@ def main():
     weights[2] = 1
     weights[5] = 1
     weights[8] = 1
-    print(grdmse(weights))
+    print("Initial Error: ", mse(weights))
+    for i in range(0, 2000):
+        weights = weights - 0.2 * grdmse(weights)
+    print("Verbesserter Error: ", mse(weights))
+    print(xor_net(0, 0, weights))
+    print(xor_net(0, 1, weights))
+    print(xor_net(1, 0, weights))
+    print(xor_net(1, 1, weights))
     
     
 if __name__ == "__main__":
