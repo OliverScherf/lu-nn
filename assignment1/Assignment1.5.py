3ca81ce1f3cbae01d663fff30ea84875085fc026
MMB2 <git@mmb2.click>
2018-03-12 14:22:35 +0100

    [:page_facing_up:] add different weight and learning rate tries

--- assignment1/Assignment1.5.py
+++ assignment1/Assignment1.5.py
@@ -20,6 +20,8 @@ from matplotlib.pyplot import plot
 from Utils import plot_confusion_matrix
 from numpy.matlib import rand
 
+global activationFunc
+
 def sigmoid(n):
     return 1.0/(1.0+np.exp(-n))
 
@@ -35,7 +37,8 @@ def linearRectifier(n):
     #    return n
 
 def activationValue(x1,x2, weights):
-    return linearRectifier(np.sum([x1, x2] * weights[0:2]) + weights[2])
+    global activationFunc
+    return activationFunc(np.sum([x1, x2] * weights[0:2]) + weights[2])
         
 def xor_net(x1, x2, weights):
     hidden1 = activationValue(x1, x2, weights[0:3])
@@ -70,21 +73,62 @@ def mse(weights):
     for i in range(0, len(xorVector)):
         error += calculateError(xMatrix[0][i], xMatrix[1][i], xorVector[i], weights)
     return error
-        
+
+
+def getMseFor(weights, ac, learningRate):
+    global activationFunc
+    activationFunc = ac
+    achievedMse = []
+    print("Initial Error: ", mse(weights))
+    achievedMse.append(mse(weights))
+    for i in range(0, 2000):
+        weights = weights - learningRate * grdmse(weights)
+        achievedMse.append(mse(weights))
+    return achievedMse
+
+
+def plotMseFor(weights, weightName, learningRate):
+    plt.figure()
+    plt.plot(getMseFor(copy.copy(weights), sigmoid, learningRate), "-", label="Sigmoid", alpha=0.5)
+    plt.plot(getMseFor(copy.copy(weights), hyperbolicTangent, learningRate), "-.", label="Hyperbolic Tangent", alpha=0.5)
+    plt.plot(getMseFor(copy.copy(weights), linearRectifier, learningRate), "--", label="Linear Rectifier", alpha=0.5)
+    plt.legend()
+    plt.xlabel("Iterations")
+    plt.ylabel("MSE")
+    plt.title("Learning Rate " + str(learningRate) + ", Weight Initialization Strategy: " + weightName)
+    plt.savefig("mse_" + weightName + "_" + str(learningRate) + ".png")
+
 
 def main():
     weights = np.random.rand(9)
     weights[2] = 1
     weights[5] = 1
     weights[8] = 1
-    print("Initial Error: ", mse(weights))
-    for i in range(0, 2000):
-        weights = weights - 0.1 * grdmse(weights)
-    print("Verbesserter Error: ", mse(weights))
-    print(xor_net(0, 0, weights))
-    print(xor_net(0, 1, weights))
-    print(xor_net(1, 0, weights))
-    print(xor_net(1, 1, weights))
+    plotMseFor(weights, "random except biasses are 1", 0.1)
+    plotMseFor(weights, "random except biasses are 1", 0.5)
+    plotMseFor(weights, "random except biasses are 1", 1)
+    plotMseFor(weights, "random except biasses are 1", 2)
+    weights = np.ones(9)
+    plotMseFor(weights, "all 1", 0.1)
+    plotMseFor(weights, "all 1", 0.5)
+    plotMseFor(weights, "all 1", 1)
+    plotMseFor(weights, "all 1", 2)
+    weights = np.full(9, 0.5)
+    weights[2] = 1
+    weights[5] = 1
+    weights[8] = 1
+    plotMseFor(weights, "all 0.5 except biasses are 1", 0.1)
+    plotMseFor(weights, "all 0.5 except biasses are 1", 0.5)
+    plotMseFor(weights, "all 0.5 except biasses are 1", 1)
+    plotMseFor(weights, "all 0.5 except biasses are 1", 2)
+    weights = np.zeros(9)
+    weights[2] = 1
+    weights[5] = 1
+    weights[8] = 1
+    plotMseFor(weights, "all 0 except biasses are 1", 0.1)
+    plotMseFor(weights, "all 0 except biasses are 1", 0.5)
+    plotMseFor(weights, "all 0 except biasses are 1", 1)
+    plotMseFor(weights, "all 0 except biasses are 1", 2)
     
     
 if __name__ == "__main__":
